6b20cd0165ca46853bf39c5627d8f30e88c37c25
bdl101 <blake.lipson.dev@gmail.com>
2022-01-27 11:35:33 -0500

    initial setup with routing

---
+++ client/src/Commitment.js
@@ -0,0 +1,11 @@
+// Display a commitment that a user can accept
+
+// TODO: show commitment details
+// TODO: allow a user to accept a commitment
+// TODO: add the accepted commitment to a user's calendar (can be an ical, gcal event etc.)
+
+const Commitment = () => {
+  return <h1>Commitment</h1>;
+};
+
+export default Commitment;
---
+++ client/src/Home.js
@@ -0,0 +1,12 @@
+// a user's home screen after they register/login
+
+// TODO: show a list of upcoming commitments
+// TODO: simulate a browser notification that leads the user to an upcoming event
+
+// TODO: navbar with settings? (stretch goal)
+
+const Home = () => {
+  return <h1>Home</h1>;
+};
+
+export default Home;
