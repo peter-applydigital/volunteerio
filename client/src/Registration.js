5688b4bb38a2aa458160fb330198df5042e4bcd8
bdl101 <blake.lipson.dev@gmail.com>
2022-01-27 11:42:59 -0500

    initial setup with routing (#1)

---
+++ client/src/Registration.js
@@ -0,0 +1,10 @@
+// screen where the user registers
+
+// TODO: add form fields that showcase the various skills/preferences/availability of a user
+// TODO: send the user to the home screen after they register
+
+const Registration = () => {
+  return <h1>Registration</h1>;
+};
+
+export default Registration;
