d94957dcf62caacfc051744fc96ba0ce0c337b54
bdl101 <blake.lipson.dev@gmail.com>
2022-01-27 15:12:29 -0500

    updated nav, theme, and added notifications

---
+++ client/src/muiTheme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#DD6031",
+    },
+    secondary: {
+      main: "#3C787E",
+    },
+  },
+});
